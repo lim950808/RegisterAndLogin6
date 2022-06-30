cfc9bedece9ec509825eb9f6efa65cd70b74a556
lim950808 <lim080895@gmail.com>
2022-07-01 00:33:50 +0900

    초기 커밋.

---
+++ RegisterAndLogin6/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+using RegisterAndLogin6.Models;
+
+namespace RegisterAndLogin6.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Login
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString))
+            {
+                string sqlQuery = "INSERT INTO User (Username, UserId, UserPassword) VALUES(" + user.UserName + "," + user.UserId + "," + user.UserPassword + ")";
+                int insert = db.Execute(sqlQuery);
+            }
+
+            return View("Login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new SqlConnection())
+                {
+                    
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
