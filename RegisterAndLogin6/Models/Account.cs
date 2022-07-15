0273f1a617a25edf92f25483f3386ff09af45701
lim950808 <lim080895@gmail.com>
2022-07-16 00:12:22 +0900

    Board View 생성 및 Account Model 수정

--- RegisterAndLogin6/Models/Account.cs
+++ RegisterAndLogin6/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,17 +8,19 @@ namespace RegisterAndLogin6.Models
 {
     public class Account
     {
-        public string Id { get; set; } //PK
+        public int Id { get; set; } //PK
 
         public string UserId { get; set; } //아이디
 
         public string Email { get; set; } //이메일
 
-        public string Password { get; set; } //비밀번호
+        public string Password { get; set; } //비밀번호 (단방향 암호화)
 
-        public Nullable<bool> EmailVerification { get; set; } //이메일 인증 여부
+        public bool? EmailVerification { get; set; } //회원가입시 이메일 인증 여부
 
-        public Nullable<System.Guid> ActivationCode { get; set; } //인증코드
+        public Guid? ActivationCode { get; set; } //암호화된 코드번호
+
+        public string OTP { get; set; } //OTP 인증코드 4자리
 
         public bool RememberMe { get; set; } //Remember me?
     }
